9223b0f363bcce515d86c170f818478f0bb5a33c
ErickS09 <josiels88@gmail.com>
2022-02-05 20:33:39 -0600

    Button

---
+++ src/components/Button/Button.js
@@ -0,0 +1,16 @@
+import styles from './button.module.css'
+
+
+function Button ( {title, type} ){
+    
+    return(
+        <button className={`${styles.button} ${styles[type]}`}>
+            {title}
+        </button>
+    )
+}
+export default Button
+Button.defaultProps = {
+    title:'Title',
+    type:'primary'
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import HeroSection from '../sections/home/HeroSection/HeroSection'
+import { Button } from '../components'
 
 function Home() {
     return (
         <div>
             <HeroSection/>
+            <Button type='primary'/>
+            <Button type='secondary'/>
         </div>
     )
 }
