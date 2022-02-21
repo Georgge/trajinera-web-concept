a6c5787b40d73403f9c6f1fc367dbe1b1c30b0b7
Georgge <jo123.garcia.s@gmail.com>
2022-02-21 17:02:17 -0600

    update button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,26 @@ import { Button, Icon } from '../components'
 function Home() {
     return (
         <div >
-            <HeroSection/>
-            <Button type='primary'/> 
+            <HeroSection />
+            <Button type='primary' />
             <Icon />
             <Icon icon="" type="brand" color="ligthSilver" size={42} />
             <Icon icon="" color="primaryBlack" />
             <Icon icon="" color="ligthPrimary" />
             <Icon icon="" color="neutralGray" />
+            <div>
+                <Button type="primary" size="auto-size" />
+                <br />
+                <Button type="primary" size="small" />
+                <br />
+                <Button type="primary" size="large" />
+                <br />
+                <Button type="primary" iconPrefix="" />
+                <br />
+                <Button type="primary" size="large" iconSufix="" />
+            </div>
         </div>
-        
+
     )
 }
 export default Home
