1a0acc4eb4a9090c193975fce2b2071694b6601a
Diego Andrei <mrdiego006@gmail.com>
2022-03-01 20:04:59 -0600

    [NUM-20]
    Created new component called Promotion

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from './Button/Button';
 export { default as Icon } from './Icon/Icon';
 export { default as ActivityCard } from './ActivityCard/ActivityCard';
+export { default as Promotion } from './Promotion/Promotion'
